6534c37d3ac329acacced755d497382dd15e6143
Paul Morganthall <paul@morganthall.com>
2012-11-26 17:50:35 -0500

    collection_select for modality

--- app/models/modality.rb
+++ app/models/modality.rb
@@ -1,3 +1,7 @@
 class Modality < ActiveRecord::Base
   attr_accessible :name, :standard
+
+  def to_s
+    name
+  end
 end
--- app/models/practitioner.rb
+++ app/models/practitioner.rb
@@ -1,4 +1,4 @@
 class Practitioner < ActiveRecord::Base
-  attr_accessible :modality, :name
+  attr_accessible :modality_id, :name
   belongs_to :modality
 end
