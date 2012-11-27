e6b369065c290b8677d08d88c9f2a01fe00b5ee6
Paul Morganthall <paul@morganthall.com>
2012-11-27 00:34:28 -0500

    Random names for the practitioner.
    Testers should focus on modalities, not prac names.

--- app/models/practitioner.rb
+++ app/models/practitioner.rb
@@ -1,4 +1,22 @@
 class Practitioner < ActiveRecord::Base
+  NAMES = [
+    'Marge Innovera',
+    'Haywood Jabuzoff',
+    'Claudio Vernight',
+    'C.F. Eye Care',
+    'Pikov Andropov',
+    'Warren Peace',
+    'Imelda Czechs',
+    'Bjorn A. Payne Diaz',
+    'Wilma Butfit',
+    'Barbara Ganush',
+    'Lois Steem',
+    'Rusty Steele',
+    'Denton Fender',
+    'Denton Fender',
+    'Selma Kayak',
+    ]
+
   attr_accessible :modality_id, :name, :modality_ids
 
   belongs_to :modality
@@ -6,4 +24,12 @@ class Practitioner < ActiveRecord::Base
   has_many :modalities, :through => :treatments
 
   validates_presence_of :name, :modality, :modalities
+
+  after_initialize :default_values
+
+  private
+    def default_values
+      self.name = "Dr. #{NAMES.sample}"
+    end
+
 end
