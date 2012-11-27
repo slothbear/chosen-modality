dc21957a9701ac98dc326e4d8d7a06d8ec705974
Paul Morganthall <paul@morganthall.com>
2012-11-27 17:44:38 -0500

    Save newly entered modality_other

--- app/models/practitioner.rb
+++ app/models/practitioner.rb
@@ -27,10 +27,18 @@ class Practitioner < ActiveRecord::Base
   validates_presence_of :name, :modality, :modalities
 
   after_initialize :default_values
+  before_save :create_new_modality
 
   private
     def default_values
       self.name = name || "Dr. #{NAMES.sample}"
+    end
+
+    # FIX: Modality should be in charge of creating modalities, not us.
+    def create_new_modality
+      if modality_other.present?
+        self.modality = Modality.find_or_create_by_name(modality_other)
+      end
     end
 
 end
