2fb6f49866211a92f5752cd8d9430b637cedcec1
Paul Morganthall <paul@morganthall.com>
2012-11-26 23:49:47 -0500

    Move "other modality…" logic into model.

--- app/models/modality.rb
+++ app/models/modality.rb
@@ -3,9 +3,14 @@ class Modality < ActiveRecord::Base
   validates_presence_of :name
 
   scope :standard, where(:standard => true).order("name")
+  scope :other, where(['name = ?', 'other modality...'])
 
   def to_s
     name
+  end
+
+  def self.with_other
+    self.standard << self.other.first
   end
 
 end
