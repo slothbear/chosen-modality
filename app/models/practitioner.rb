a1440abd6fa58c0ab694cf461f543fbb09b864bb
Paul Morganthall <paul@morganthall.com>
2012-11-28 15:42:22 -0500

    Shuttle new modalities into a separate field.
    and create them before_save

--- app/models/practitioner.rb
+++ app/models/practitioner.rb
@@ -17,8 +17,9 @@ class Practitioner < ActiveRecord::Base
     'Selma Kayak',
     ]
 
-  attr_accessible :modality_id, :name, :modality_ids, :modality_other
-  attr_accessor :modality_other
+  attr_accessible :modality_id, :name, :modality_ids, :modality_other, :new_modalities
+
+  attr_accessor :modality_other, :new_modalities
 
   belongs_to :modality
   has_many :treatments, :dependent => :destroy
@@ -27,7 +28,8 @@ class Practitioner < ActiveRecord::Base
   validates_presence_of :name, :modality, :modalities
 
   after_initialize :default_values
-  before_save :create_new_modality
+  before_save :create_other_modality
+  before_save :create_new_modalities
 
   private
     def default_values
@@ -35,10 +37,22 @@ class Practitioner < ActiveRecord::Base
       self.modality = modality || Modality.standard.sample
     end
 
-    # FIX: Modality should be in charge of creating modalities, not us.
-    def create_new_modality
+    # FIX: (TELL¬ASK) Modality should be in charge of creating modalities.
+    # FIX: use guard/return like #create_new_modalities?
+    def create_other_modality
       if modality_other.present?
         self.modality = Modality.find_or_create_by_name(modality_other)
+      end
+    end
+
+    # FIX: (TELL¬ASK) Modality should be in charge of creating modalities, not us.
+    def create_new_modalities
+      return unless new_modalities.present?
+
+      new_modalities.each do |modality_name|
+        if modality_name.present?
+          self.modalities << Modality.find_or_create_by_name(modality_name)
+        end
       end
     end
 
