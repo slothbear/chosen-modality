82c7578117ae079c6c04a6e820bd70a7fb8bba3a
Paul Morganthall <paul@morganthall.com>
2012-11-29 12:53:46 -0500

    Replace :standard scope with simple #order.

--- app/models/modality.rb
+++ app/models/modality.rb
@@ -2,15 +2,10 @@ class Modality < ActiveRecord::Base
   attr_accessible :name, :standard
   validates_presence_of :name
 
-  scope :standard, where(:standard => true).order("name")
-  scope :other, where(['name = ?', 'other modality...'])
+  scope :standard, order(:name)
 
   def to_s
     name
-  end
-
-  def self.with_other
-    self.standard << self.other.first
   end
 
   def self.isolate_new_modalities(params)
