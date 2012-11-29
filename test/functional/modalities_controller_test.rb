64c4861b805b7cb6798786f1cd6d42fd00c0479b
Paul Morganthall <paul@morganthall.com>
2012-11-29 14:02:42 -0500

    Remove generated tests, relocation isolate_ tests

--- test/functional/modalities_controller_test.rb
+++ test/functional/modalities_controller_test.rb
@@ -1,49 +1,37 @@
 require 'test_helper'
 
 class ModalitiesControllerTest < ActionController::TestCase
-  setup do
-    @modality = modalities(:one)
+
+  test "separate new and existing modalities" do
+    params = {:modality_ids => ["42", "Sleep Therapy"]}
+    @controller.send(:isolate_new_modalities, isolate_new_modalities, params)
+
+    assert_equal ["42"], params[:modality_ids]
+    assert_equal ["Sleep Therapy"], params[:new_modalities]
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:modalities)
+  test "multiple old and new modalities" do
+    params = {:modality_ids => ["3", "Hot Yoga", "Diet", "18"]}
+    @controller.send(:isolate_new_modalities, isolate_new_modalities, params)
+
+    assert_equal ["3", "18"], params[:modality_ids]
+    assert_equal ["Hot Yoga", "Diet"], params[:new_modalities]
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
+  test "single new modality" do
+    params = {:modality_ids => ["Cupping"]}
+    @controller.send(:isolate_new_modalities, isolate_new_modalities, params)
+
+    assert_equal [], params[:modality_ids]
+    assert_equal ["Cupping"], params[:new_modalities]
   end
 
-  test "should create modality" do
-    assert_difference('Modality.count') do
-      post :create, modality: { name: @modality.name, standard: @modality.standard }
-    end
+  test "handle empty modalities" do
+    params = {:modality_ids => []}
+    @controller.send(:isolate_new_modalities, isolate_new_modalities, params)
 
-    assert_redirected_to modality_path(assigns(:modality))
+    assert_equal [], params[:modality_ids]
+    assert_equal [], params[:new_modalities]
   end
 
-  test "should show modality" do
-    get :show, id: @modality
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @modality
-    assert_response :success
-  end
-
-  test "should update modality" do
-    put :update, id: @modality, modality: { name: @modality.name, standard: @modality.standard }
-    assert_redirected_to modality_path(assigns(:modality))
-  end
-
-  test "should destroy modality" do
-    assert_difference('Modality.count', -1) do
-      delete :destroy, id: @modality
-    end
-
-    assert_redirected_to modalities_path
-  end
 end
