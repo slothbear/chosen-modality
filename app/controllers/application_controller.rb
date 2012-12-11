86b0407afd76ef11d7d77df2196fd95758905e56
Paul Morganthall <paul@morganthall.com>
2012-12-11 12:40:12 -0500

    Document thanks to Sarah Mei.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,8 +18,11 @@ class ApplicationController < ActionController::Base
     arr.reject!(&:empty?)
   end
 
+  # TODO: [THANKS] To Sarah Mei [1].
   def integer?(value)
     value.to_i.to_s == value
   end
 
 end
+
+# [1] http://stackoverflow.com/questions/1235863/test-if-a-string-is-basically-an-integer-in-quotes-using-ruby
